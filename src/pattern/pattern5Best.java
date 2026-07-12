0b946bf1f7cb07b4778d8c290a2e6a230e805584
KalendraYadav <kalendrak527@gmail.com>
2026-07-12 15:16:11 +0530

    some hard pattern

--- src/pattern/pattern5Best.java
+++ src/pattern/pattern5Best.java
@@ -1,9 +1,17 @@
+package pattern;
 import java.util.Scanner;
+
+// * 
+// * * 
+// * * * 
+// * * * * 
+// * * * * * 
 
 public class pattern5Best {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
+        sc.close();
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
                 System.out.print("* ");
