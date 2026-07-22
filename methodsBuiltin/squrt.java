8769970c1a73e4082695ba9474a76f62103be5c3
krcraft8373-hash <krcraft8373@gmail.com>
2026-07-22 15:10:39 +0530

    worked on some function code

---
+++ methodsBuiltin/squrt.java
@@ -0,0 +1,25 @@
+public class squrt {
+    public static void main(String[]args){
+        double x= Math.sqrt(5);
+        System.out.println(x);
+        System.out.println("the square root of 9 is: " + Math.sqrt(9));
+        System.out.println("the cube root of 100 is: " + Math.cbrt(100));
+        System.out.println("the value of PI is: " + Math.PI);
+        // System.out.println(Math.abs(-9));
+        // System.out.println(Math.max(9, 5));
+        // System.out.println(Math.min(9, 5));
+        // System.out.println(Math.floor(4.5));
+        // System.out.println(Math.ceil(4.5));
+        System.out.println(Math.min(5.1254,18));
+        System.out.println(Math.max(5, Math.max(8,9)));
+        System.out.println(Math.max(Math.max(4,5), Math.max(8,9)));
+        System.out.println(Math.pow(6, 3));
+        System.out.println(Math.pow(-6, -3));
+        System.out.println(Math.pow(-6.254, -3.255));
+        System.out.println(Math.pow(-6.255, -3));
+        System.out.println(Math.pow(6.2554, 3.2541));
+        System.out.println(Math.pow(0, 0));
+
+
+    }
+}
