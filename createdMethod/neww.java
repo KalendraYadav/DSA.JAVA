4e1949571744d0a4e4c35205a9323e654583ddad
KalendraYadav <kalendrak527@gmail.com>
2026-07-23 11:19:40 +0530

    worked on function

---
+++ createdMethod/neww.java
@@ -0,0 +1,16 @@
+package createdMethod;
+public class neww{
+
+    public static void kalendra(){
+        System.out.println("hello i am kalendra");
+    }
+    public static void main(String[]args){
+        System.out.println("hello i am main method");
+        kalendra();
+        hari();
+
+    }
+  public static void hari(){
+    System.out.println("hello i am hari");
+  }
+}
--- src/basic/surfaceAreaCuboid.java
+++ src/basic/surfaceAreaCuboid.java
@@ -7,7 +7,20 @@ public class surfaceAreaCuboid {
         System.out.println(tsa);
         int $hii=9;          //variable name can start with $ and _ but not with number or other special character.
         int _hii=19;
-        int $420=420;
+        int $420=420;package basic;
+
+public class variables {
+   public  static void main(String[] args) {
+        int x =9;
+        System.out.println(x);
+        System.out.println(x+7);
+        x=x*2;
+        System.out.println(x);
+        x=x-18;
+       System.out.println(x);
+    }
+}
+
 
         System.out.println("_hii is "+ _hii);
         System.out.println("$hii is " + $hii);
