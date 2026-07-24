ff3fd309adf0bf2e92500446a75b3a858b07ad2a
KalendraYadav <kalendrak527@gmail.com>
2026-07-24 21:47:51 +0530

    done few question on the array

---
+++ ARRAY/arrayBynew.java
@@ -0,0 +1,13 @@
+package ARRAY;
+
+public class arrayBynew {
+    public static void main(String[] args) {
+        int [] arr= new int[4];
+        arr[0]=9;
+        arr[2]=12;
+        arr[1]=-6;
+        arr[3]=4;
+        System.out.println(arr[0]);
+    
+}
+}
---
+++ ARRAY/arraybasic.java
@@ -0,0 +1,15 @@
+package ARRAY;
+
+public class arraybasic {
+    public static void main(String[]args){
+        int [] x= {12,1,45,0,23,4,7,8};
+        System.out.println(x[0]);
+        int[] n={1,2,5,6,4,78,65,2,0};
+        System.out.println(n[4]);
+
+        //updating array
+        n[4]=12;
+        System.out.println(n[4]);
+    }
+    
+}
