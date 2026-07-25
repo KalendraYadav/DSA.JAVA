d88891a8c5abbc84ff1757f0991668f06d8643f9
KalendraYadav <kalendrak527@gmail.com>
2026-07-25 11:49:58 +0530

    printing the array through for loop

---
+++ ARRAY/array2.java
@@ -0,0 +1,18 @@
+package ARRAY;
+public class array2{
+    public static void main(String[]args){
+        int[] arr={2,5,6,8,9,-7,41};
+        // System.out.println(arr[0]);
+        // System.out.println(arr[1]);
+        // System.out.println(arr[2]);
+        // System.out.println(arr[3]);
+        // System.out.println(arr[4]);
+        // System.out.println(arr[5]);
+        // System.out.println(arr[6]);      
+
+        for(int i=0;i<7;i++)
+            System.out.print(arr[i]+"  ");
+        System.out.println();
+        System.out.println(arr.length);
+    }
+}
