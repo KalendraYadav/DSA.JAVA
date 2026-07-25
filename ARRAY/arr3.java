127d53c2a5abb622cfe39fe62dcc330533859998
KalendraYadav <kalendrak527@gmail.com>
2026-07-25 13:41:32 +0530

    taking input of the array

---
+++ ARRAY/arr3.java
@@ -0,0 +1,12 @@
+package ARRAY;
+
+public class arr3 {
+    
+    public static void main(String[]args){
+
+        int [] karan= new int[6];
+        for (int i=0;i<karan.length;i++){
+         System.out.print(karan[i]+"  ");
+        }
+    
+}}
