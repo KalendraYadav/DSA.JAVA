30f3a5aac97ff05da62a5420ccfebde76c878242
KalendraYadav <kalendrak527@gmail.com>
2026-07-26 13:38:54 +0530

    array pass by refrence

---
+++ passingArrayToFunction/arraypass.java
@@ -0,0 +1,15 @@
+package passingArrayToFunction;
+
+public class arraypass {
+    public static void main(String[]args){
+  
+        int [] arr={1,2,3,4,5};
+        System.out.println(arr[2]);
+        change(arr);
+        System.out.println(arr[2]);
+    }
+    public static void change(int[]brr){
+        brr[2]=99;
+    }
+    
+}
