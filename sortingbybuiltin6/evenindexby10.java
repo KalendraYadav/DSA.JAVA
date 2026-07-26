e9544ab737fc1ed64a077dcc457c7cfad717457f
KalendraYadav <kalendrak527@gmail.com>
2026-07-27 00:56:19 +0530

    multiply the odd index by 2

---
+++ sortingbybuiltin6/evenindexby10.java
@@ -0,0 +1,21 @@
+package sortingbybuiltin6;
+
+public class evenindexby10 {
+    public static void main(String[] args){
+        int [] arr={1,2,3,4,5,6,7,8,9,10};
+        for(int i=0;i<arr.length;i++){
+            if(i%2!=0){
+                arr[i]=arr[i]*2;
+            }
+            else{
+                arr[i]=arr[i]+10;
+            }
+        }
+
+        System.out.println("printing the new array");
+        for(int i=0;i<arr.length;i++){
+            System.out.print("  "+arr[i]);
+        }
+    }
+    
+}
