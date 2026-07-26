abd7ccd9164f71461c0ad302a2ddddd8a06544f4
KalendraYadav <kalendrak527@gmail.com>
2026-07-26 10:32:37 +0530

    double type array

---
+++ ARRAYotherDatatype/doubleedefault.java
@@ -0,0 +1,11 @@
+package ARRAYotherDatatype;
+
+public class doubleedefault {
+    public static void main(String[]args){
+        double[] arr=new double [6];
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+    
+}
---
+++ ARRAYotherDatatype/takedouble.java
@@ -0,0 +1,15 @@
+package ARRAYotherDatatype;
+import java.util.Scanner;
+public class takedouble {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        double [] lrr=new double [6];
+        for(int i=0;i<lrr.length;i++){
+            lrr[i]=sc.nextDouble();
+        }
+
+        for(int i=0;i<lrr.length;i++){
+            System.out.print("  "+lrr[i]);
+        }
+    }
+}
