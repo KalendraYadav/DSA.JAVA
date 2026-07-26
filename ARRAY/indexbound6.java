6fa96f0a40f192f7b181db51ee258001e7b81071
KalendraYadav <kalendrak527@gmail.com>
2026-07-26 10:14:27 +0530

    array out of bound

---
+++ ARRAY/indexbound6.java
@@ -0,0 +1,12 @@
+package ARRAY;
+import java.util.Scanner;
+public class indexbound6 {
+    public static void main(String[]arga){
+        Scanner sc=new Scanner(System.in);
+        int arr[]=new int[5];
+        for(int i=-1;i<arr.length;i++){
+            arr[i]= sc.nextInt();
+        }
+    
+}
+}
---
+++ ARRAY/indexoutofbound5.java
@@ -0,0 +1,11 @@
+package ARRAY;
+
+public class indexoutofbound5 {
+    public static void main(String[]args){
+        int[ ] arr={1,2,3,4,5};
+        // System.out.println(arr[-5]);
+        arr[-1]=10;
+        System.out.println(arr[-1]);
+    }
+    
+}
