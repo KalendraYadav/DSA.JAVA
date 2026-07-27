75c722c558c85bff9cf88cc041dd81e829694d52
KalendraYadav <kalendrak527@gmail.com>
2026-07-28 01:33:05 +0530

    two sum in java

---
+++ TWOsum8/twosum1.java
@@ -0,0 +1,24 @@
+package TWOsum8;
+
+public class twosum1 {
+    public static void main(String[] args){
+        int[] hrr={4,58,2,1,10,56,-9,-8,-45,1,2};
+        int target=8;
+        boolean flag = false;
+        for(int i=0;i<hrr.length;i++){
+            
+            for(int j=i+1;j<hrr.length;j++){
+                if(hrr[i]+hrr[j]==target){
+                    System.out.println("the two numbers are "+hrr[i]+" and "+hrr[j]);
+                    flag= true;
+                    break;
+                }
+
+            }
+        }
+        if(flag==false){
+            System.out.println("no such numbers found");
+        }
+    }
+    
+}
