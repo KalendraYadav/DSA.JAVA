ce4fa9336f465b5a5433c055c4c1ce124150ba8b
KalendraYadav <kalendrak527@gmail.com>
2026-08-02 10:39:13 +0530

    second max failed when max appear for more then once

--- DSApractice/secondmaxSorting4.java
+++ DSApractice/secondmaxSorting4.java
@@ -2,13 +2,13 @@ package DSApractice;
 import java.util.Arrays;
 public class secondmaxSorting4 {
     public static void main(String[] args) {
-        // int [] arr={7,-8,4,-2,3,6,-9,45,23,7};
-        int [] arr={5,5,5,5,5,5,5,5};
+        int [] arr={7,-8,4,-2,3,6,-9,45,23,45,7};
+        // int [] arr={5,5,5,5,5,5,5,5};
 
         System.out.println(sec(arr));
     }
 
-    public static int sec(int arr[]){
+    public static int sec(int arr[]){   //method will fail when the maximum element will appear more then once.
         Arrays.sort(arr);
         return arr[arr.length-2];
     }
