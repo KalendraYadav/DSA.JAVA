185de8888b4d048f6bd636e98520019bc64e5ed2
KalendraYadav <kalendrak527@gmail.com>
2026-08-02 10:12:00 +0530

    two sum return in boolean

---
+++ DSApractice/twosumboolean3.java
@@ -0,0 +1,26 @@
+//time limit exceeded in this code.
+package DSApractice;
+
+public class twosumboolean3 {
+    public static void main(String[] args) {
+        int [] arr={4,-5,6,5,-8,2};
+        int target=6;
+       boolean ans= twosum(arr,target);
+        // System.out.println(twosum(arr,target));
+        System.out.println(ans);
+    }
+
+    public static boolean twosum(int arr[] , int target){
+        int found=0;
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length;j++){
+                if(arr[i]+arr[j]==target){
+                    found=1;
+                }
+            }
+        }
+        if(found==0) return false;
+        else return true;
+    }
+    
+}
