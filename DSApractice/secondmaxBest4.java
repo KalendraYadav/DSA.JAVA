d70c5a0942299a04a5cdc8d6a4bb97a1aaaef938
KalendraYadav <kalendrak527@gmail.com>
2026-08-02 11:37:57 +0530

    fixed second max

--- DSApractice/secondmaxBest4.java
+++ DSApractice/secondmaxBest4.java
@@ -5,19 +5,21 @@ public class secondmaxBest4 {
         int [] arr={50,50,49,7,-8,4,-2,3,6,-9,45,23,23,22,45,7};
         System.out.println(best(arr)); 
     }
+    //will fail in case if the first two elements will be largest.
 
     public static int best(int []arr){
-        int max=arr[0];
-        int smax=arr[1];
+        int max=Integer.MIN_VALUE;  //we can not store 0 th or 1th element here as if the
+        //largest repeated element will be in the starting then the loop will not run even once.
+        int smax=Integer.MIN_VALUE;
         for(int i=0;i<arr.length;i++){
             if(arr[i]>max){
-                smax=max;
                 max=arr[i];
             }
-            else if(arr[i]>smax && arr[i]<max){
+        }
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>smax && arr[i]<max){
                 smax=arr[i];
             }
-
         }
         return smax;
     }
