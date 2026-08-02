5b34381117368ddaf4c68ac6ce95739ef1032b40
KalendraYadav <kalendrak527@gmail.com>
2026-08-02 10:29:24 +0530

    second large by sorting

---
+++ DSApractice/secondmaxSorting4.java
@@ -0,0 +1,16 @@
+package DSApractice;
+import java.util.Arrays;
+public class secondmaxSorting4 {
+    public static void main(String[] args) {
+        // int [] arr={7,-8,4,-2,3,6,-9,45,23,7};
+        int [] arr={5,5,5,5,5,5,5,5};
+
+        System.out.println(sec(arr));
+    }
+
+    public static int sec(int arr[]){
+        Arrays.sort(arr);
+        return arr[arr.length-2];
+    }
+    
+}
