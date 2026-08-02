675044871d72f5b21544a3f973a2a13a19a50680
KalendraYadav <kalendrak527@gmail.com>
2026-08-02 20:52:34 +0530

    sort zero

---
+++ CHAPTER8/foreachloop1.java
@@ -0,0 +1,7 @@
+package CHAPTER8;
+public class foreachloop1 {
+    public static void main(String[] args) {
+        
+    }
+        
+}
---
+++ DSApractice/sortZerotoend5.java
@@ -0,0 +1,22 @@
+package DSApractice;
+
+public class sortZerotoend5 {
+    public static void main(String[] args) {
+        int [] arr={4,5,0,0,0,3,12};
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]==0){
+                    int temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+
+            }
+        }
+
+        for(int i=0;i<arr.length;i++){
+          System.out.print(" "+arr[i]);
+        }
+    }
+    
+}
